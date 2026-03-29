ce6c73b1baeb0677ef34cc67336b676caba7965e
Marco <marco93mendes@gmail.com>
2026-03-28 21:48:40 -0300

    Create data class for model Contact

---
+++ app/src/main/java/com/piomendes/contactsapp/data/Contact.kt
@@ -0,0 +1,10 @@
+package com.piomendes.contactsapp.data
+
+data class Contact(
+    val id: Int = 0,
+    val firstName: String,
+    val lastName: String,
+    val phoneNumber: String,
+    val email: String,
+    val isFavorite: Boolean = false
+)
