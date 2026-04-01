5dcac420c6ada1dcd396c400917dded20c4c8602
Marco <marco93mendes@gmail.com>
2026-03-31 21:05:31 -0300

    Add dependencies to navigation and use the function to create viewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,9 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.material.icons.extended)
 
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
