3d915f4fb2178a4f20da5c37c40614a330239cb6
Marco <marco93mendes@gmail.com>
2026-04-07 18:59:49 -0300

    New properties at model Contact

--- app/src/main/java/com/piomendes/contactsapp/data/Contact.kt
+++ app/src/main/java/com/piomendes/contactsapp/data/Contact.kt
@@ -1,16 +1,36 @@
 package com.piomendes.contactsapp.data
 
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+enum class ContactTypeEnum {
+    PERSONAL,
+    PROFESSIONAL
+}
+
 data class Contact(
     val id: Int = 0,
     val firstName: String,
     val lastName: String,
     val phoneNumber: String = "",
     val email: String = "",
-    val isFavorite: Boolean = false
+    val isFavorite: Boolean = false,
+    val createdAr: LocalDateTime = LocalDateTime.now(),
+    val birthDate: LocalDate = LocalDate.now(),
+    val type: ContactTypeEnum = ContactTypeEnum.PERSONAL,
+    val assetValue: BigDecimal = BigDecimal.ZERO
 ) {
     val fullName get() = "$firstName $lastName".trim()
 }
 
+
 fun List<Contact>.groupByInitial() : Map<String, List<Contact>> {
     return sortedBy { it.fullName } .groupBy { it.fullName.take(1) }
+}
+
+fun LocalDate.format(): String {
+    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    return format(formatter)
 }
