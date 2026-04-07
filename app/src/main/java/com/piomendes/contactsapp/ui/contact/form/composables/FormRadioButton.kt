ec1d5b66d4aa92948cc0b07c9f43efd651c26f43
Marco <marco93mendes@gmail.com>
2026-04-07 20:32:01 -0300

    Created FormTadioButton component

---
+++ app/src/main/java/com/piomendes/contactsapp/ui/contact/form/composables/FormRadioButton.kt
@@ -0,0 +1,70 @@
+package com.piomendes.contactsapp.ui.contact.form.composables
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.RadioButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.piomendes.contactsapp.data.ContactTypeEnum
+import com.piomendes.contactsapp.ui.theme.ContactsAppTheme
+
+@Composable
+fun FormRadioButton(
+    modifier: Modifier = Modifier,
+    label: String,
+    value: ContactTypeEnum,
+    groupVale: ContactTypeEnum,
+    onValueChanged: (ContactTypeEnum) -> Unit,
+    enabled: Boolean = true
+) {
+    Row(
+      modifier = modifier
+          .clickable { onValueChanged(value) },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(
+            selected = value == groupVale,
+            onClick = { onValueChanged(value) },
+            enabled = enabled
+        )
+        Text(label)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FormRadioButtonPreview() {
+    ContactsAppTheme {
+        var groupValue by remember { mutableStateOf(ContactTypeEnum.PROFESSIONAL) }
+        Column {
+            FormRadioButton(
+                modifier = Modifier.padding(20.dp),
+                label = "Personal",
+                value = ContactTypeEnum.PERSONAL,
+                groupVale = groupValue,
+                onValueChanged = { newValue ->
+                    groupValue = newValue
+                }
+            )
+            FormRadioButton(
+                modifier = Modifier.padding(20.dp),
+                label = "Professional",
+                value = ContactTypeEnum.PROFESSIONAL,
+                groupVale = groupValue,
+                onValueChanged = { newValue ->
+                    groupValue = newValue
+                }
+            )
+        }
+    }
+}
